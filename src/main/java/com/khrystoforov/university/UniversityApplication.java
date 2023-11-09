68bc74a040f9ffe1cc9ce6219ff959d4d36f8c7b
HrisKirill <hristoforow.kirill@gmail.com>
2023-11-09 19:29:51 +0200

    add io.hypersistence

--- src/main/java/com/khrystoforov/university/UniversityApplication.java
+++ src/main/java/com/khrystoforov/university/UniversityApplication.java
@@ -1,11 +1,18 @@
 package com.khrystoforov.university;
 
+import io.hypersistence.utils.spring.repository.BaseJpaRepositoryImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(
+        value = "com.khrystoforov.university.repository",
+        repositoryBaseClass = BaseJpaRepositoryImpl.class
+)
+@RequiredArgsConstructor
 public class UniversityApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UniversityApplication.class, args);
     }
